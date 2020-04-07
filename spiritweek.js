99cb92e158dfc9323006509c0a3374454db26b9b
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-07 17:36:52 -0400

    Added for loop and console log

--- spiritweek.js
+++ spiritweek.js
@@ -1,6 +1,6 @@
-function spiritWeek () {
-    let daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+let daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
 
     let themes = ['Crazy Hat Day', ' Animal Day', 'Matching Day', 'Fun Socks', 'Hero Day'];
     
-}
+    for (i = 0; i < daysOfWeek.length; i++)
+    console.log(`${daysOfWeek[i]} is ${themes[i]}!`);
