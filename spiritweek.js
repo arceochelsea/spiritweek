e2411bb787be59527354a6f3dd0690a486af0415
Chelsea Arceo <arceo.chelsea@gmail.com>
2020-04-07 17:21:31 -0400

    Created two arrays

---
+++ spiritweek.js
@@ -0,0 +1,6 @@
+function spiritWeek () {
+    let daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+
+    let themes = ['Crazy Hat Day', ' Animal Day', 'Matching Day', 'Fun Socks', 'Hero Day'];
+    
+}
